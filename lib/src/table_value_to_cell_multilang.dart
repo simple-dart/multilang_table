b20e56a2ffb9f140afc9a129a55bd9722a5e6949
viktorzaharov <viktorzahar@gmail.com>
2023-01-12 13:57:46 +0300

    Version 0.0.2

--- lib/src/table_value_to_cell_multilang.dart
+++ lib/src/table_value_to_cell_multilang.dart
@@ -1,5 +1,6 @@
 import 'package:simple_dart_table/simple_dart_table.dart';
 import 'package:simple_dart_ui_core/simple_dart_ui_core.dart';
+import 'package:simple_dart_utils_date_time/simple_dart_utils_date_time.dart' as utils_date_time;
 
 import 'multilang_table_cells.dart';
 
@@ -22,7 +23,7 @@ AbstractTableCell tableValueToCellMultilang(TableColumnDescr columnDescr, dynami
     } else if (value is num) {
       valueStr = value.toStringAsFixed(columnDescr.precision);
     } else if (value is DateTime) {
-      valueStr = formatDateHum(value);
+      valueStr = utils_date_time.formatDateTimeHumIfExist(value);
     } else {
       valueStr = value.toString();
     }
--- lib/src/table_value_to_string_multilang.dart
+++ lib/src/table_value_to_string_multilang.dart
@@ -1,5 +1,6 @@
 import 'package:simple_dart_multilang_controller/simple_dart_multilang_controller.dart';
 import 'package:simple_dart_table/simple_dart_table.dart';
+import 'package:simple_dart_utils_date_time/simple_dart_utils_date_time.dart' as utils_date_time;
 
 String tableValueToStringMultilang(TableColumnDescr columnDescr, dynamic value) {
   String formattedValue;
@@ -13,6 +14,8 @@ String tableValueToStringMultilang(TableColumnDescr columnDescr, dynamic value) 
     formattedValue = '';
   } else if (value is num) {
     formattedValue = value.toStringAsFixed(columnDescr.precision).replaceAll('.', ',');
+  } else if (value is DateTime) {
+    formattedValue = utils_date_time.formatDateTime(value);
   } else {
     formattedValue = multilangController.translate(value.toString());
   }
